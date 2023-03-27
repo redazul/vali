c7944855cdc422438df8af958c0ac1542df0e1fe
azuldev <91290695+redazul@users.noreply.github.com>
2023-03-26 19:01:59 -0600

    Update install-vali.sh

--- install-vali.sh
+++ install-vali.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #get systemd vali file
-curl -o /etc/systemd/system/vali-server.service https://raw.githubusercontent.com/redazul/vali/main/vali-server.service
+sudo curl -o /etc/systemd/system/vali-server.service https://raw.githubusercontent.com/redazul/vali/main/vali-server.service
 
 #get nodejs vali file
-curl -o /home/ubuntu/vali-server.js https://raw.githubusercontent.com/redazul/vali/main/vali-server.js
+sudo curl -o /home/ubuntu/vali-server.js https://raw.githubusercontent.com/redazul/vali/main/vali-server.js
 
 #start vali service
 sudo systemctl start vali-server.service
