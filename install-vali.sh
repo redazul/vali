8bf9dcec8ddb770f3d3612a8bee6431de971c507
azuldev <91290695+redazul@users.noreply.github.com>
2023-03-26 20:13:51 -0600

    Update install-vali.sh

--- install-vali.sh
+++ install-vali.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #install dep
-npm i express
+npm install -g express
 
 #get systemd vali file
 sudo curl -o /etc/systemd/system/vali-server.service https://raw.githubusercontent.com/redazul/vali/main/vali-server.service
